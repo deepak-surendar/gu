7d12d57be6599b3743ec443d6f58fe3c2d24049b
Deepak <seema@Seemas-MacBook-Air.local>
2022-05-06 11:42:23 +1000

    code formatting

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,37 +11,37 @@
 //
 Cypress.Commands.add('enterSearchField', (htmlElement, searchCriteria) => {
     cy.get(htmlElement).should('be.visible')
-    .type(searchCriteria)
+        .type(searchCriteria)
 })
 
 Cypress.Commands.add('clickDropdown', (htmlElement) => {
     cy.get(htmlElement).should('be.visible')
-            .click()
+        .click()
 })
 
 Cypress.Commands.add('selectOption', (htmlElement, optionValue) => {
     cy.get(htmlElement).should('be.visible')
-    .each(($selectBoxesElements) => {
-        cy.wrap($selectBoxesElements).contains(optionValue)
-            .click()
-    })
+        .each(($selectBoxesElements) => {
+            cy.wrap($selectBoxesElements).contains(optionValue)
+                .click()
+        })
 })
 
 Cypress.Commands.add('search', (htmlElement) => {
     cy.get(htmlElement).should('be.visible')
-            .click()
+        .click()
 })
 
 Cypress.Commands.add('clickFirstSearchResult', (htmlElement) => {
     cy.get('div.user-ad-collection-new-design__wrapper--row>a')
         .should('have.length.greaterThan', 0)
         .should('be.visible')
-            .first().click()
+        .first().click()
 })
 
 Cypress.Commands.add('getAdvertisementId', (htmlElement) => {
     return cy.get(htmlElement).should('be.visible')
-            .invoke('text')
+        .invoke('text')
 })
 
 Cypress.Commands.add('returnAllSimilarSearchResults', (htmlElement) => {
