71418616e142053a10c1fae3eecd4655bcb5d7ee
Deepak <seema@Seemas-MacBook-Air.local>
2022-05-06 11:52:00 +1000

    code organisation

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,17 +8,19 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
-//
+// Custom command to enter string in search box
 Cypress.Commands.add('enterSearchField', (htmlElement, searchCriteria) => {
     cy.get(htmlElement).should('be.visible')
         .type(searchCriteria)
 })
 
+// Custom command to click on dropdown
 Cypress.Commands.add('clickDropdown', (htmlElement) => {
     cy.get(htmlElement).should('be.visible')
         .click()
 })
 
+// Custom command to selece an option from a dropdown
 Cypress.Commands.add('selectOption', (htmlElement, optionValue) => {
     cy.get(htmlElement).should('be.visible')
         .each(($selectBoxesElements) => {
@@ -27,23 +29,27 @@ Cypress.Commands.add('selectOption', (htmlElement, optionValue) => {
         })
 })
 
+// Custom command to click a button, in this case, Search button
 Cypress.Commands.add('search', (htmlElement) => {
     cy.get(htmlElement).should('be.visible')
         .click()
 })
 
+// Custom command to click on First Search Result
 Cypress.Commands.add('clickFirstSearchResult', (htmlElement) => {
-    cy.get('div.user-ad-collection-new-design__wrapper--row>a')
+    cy.get(htmlElement)
         .should('have.length.greaterThan', 0)
         .should('be.visible')
         .first().click()
 })
 
+// Custom command to retreive AdvertisementId from search results
 Cypress.Commands.add('getAdvertisementId', (htmlElement) => {
     return cy.get(htmlElement).should('be.visible')
         .invoke('text')
 })
 
+// Custom command to retreive an HTML element, in this case Similar search results
 Cypress.Commands.add('returnAllSimilarSearchResults', (htmlElement) => {
     return cy.get(htmlElement)
 })
