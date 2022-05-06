2c8d9d8db77990b6d3ebf2d2b459b02196801660
Deepak <seema@Seemas-MacBook-Air.local>
2022-05-06 11:40:58 +1000

    code organisation

--- cypress/integration/gumtree_spec.js
+++ cypress/integration/gumtree_spec.js
@@ -12,56 +12,55 @@ describe('UI Test - Gumtree search and validate results', () => {
         cy.clickDropdown('div#categoryId-wrp')
 
         // select Electronics & Computer
-        cy.selectOption('div#categoryId-wrpwrapper', 'Electronics & Computer')    
+        cy.selectOption('div#categoryId-wrpwrapper', 'Electronics & Computer')
 
         // enter location
         cy.enterSearchField('#search-area', 'Sydney Region, NSW')
-        
+
         // click search radius dropdown
         cy.clickDropdown('#srch-radius-wrp')
-             
+
         // select 20Km
-        cy.selectOption('div#srch-radius-wrpwrapper', '20km')    
+        cy.selectOption('div#srch-radius-wrpwrapper', '20km')
 
         // Search (or) submit the form
         cy.search('button[type="submit"]')
 
         // click on first search result
-        cy.get('div.user-ad-collection-new-design__wrapper--row>a')
-        .should('have.length.greaterThan', 0)
-        .should('be.visible')
-            .first().click()
+        cy.clickFirstSearchResult('div.user-ad-collection-new-design__wrapper--row>a')
 
         // validate Advertisement Id using regEx, that the id ends with a numeric
-        cy.get('span.breadcrumbs__summary').should('be.visible')
-            .invoke('text')
+        cy.getAdvertisementId('span.breadcrumbs__summary')
             .should('match', /[0-9]*$/)
 
         // check if atleast one similar results are displayed in the UI
-        cy.get('ul.slider__item-wrapper>li').should('have.length.greaterThan', 0)
+        cy.returnAllSimilarSearchResults('ul.slider__item-wrapper>li')
+            .should('have.length.greaterThan', 0)
     })
 })
 
 describe('API Test - Gumtree endpoint', () => {
     it('returns search results via api', () => {
+        const endpoint = 'https://ecg-api.gumtree.com.au/api/papi/ads/search?categoryId=0&categoryRedirected=1&includeTopAds=1&keyword=Table&locationId=3003435&page=1&size=20&sortType=DATE_DESCENDING'
+
         // generate GET request
         cy.request({
-         url: 'https://ecg-api.gumtree.com.au/api/papi/ads/search?categoryId=0&categoryRedirected=1&includeTopAds=1&keyword=Table&locationId=3003435&page=1&size=20&sortType=DATE_DESCENDING',
-         method: 'GET'   
+            url: endpoint,
+            method: 'GET'
         })
-        .then((response) => {
-            // validate response headers
-            expect(response.status).to.eq(200)    
-            expect(response.headers['content-type']).to.include('application/json')
-            
-            // validate some response body fields
-            expect(response.body.ads.length).to.be.at.least(1)
-            expect(response.body.ads[0].adType).to.equal('OFFERED')
-            expect(response.body.ads[0].description.length).to.be.greaterThan(1)
-            expect(response.body.ads[0].categoryId).to.not.be.null
-            expect(response.body.ads[0].title).to.be.a('string')
-            expect(response.body.ads[0].pictures).to.not.be.empty
-            expect(response.body.ads[0].pictures[0][0]).to.have.keys(['size', 'pictureUrl'])           
-        })
+            .then((response) => {
+                // validate response headers
+                expect(response.status).to.eq(200)
+                expect(response.headers['content-type']).to.include('application/json')
+
+                // validate some response body fields
+                expect(response.body.ads.length).to.be.at.least(1)
+                expect(response.body.ads[0].adType).to.equal('OFFERED')
+                expect(response.body.ads[0].description.length).to.be.greaterThan(1)
+                expect(response.body.ads[0].categoryId).to.not.be.null
+                expect(response.body.ads[0].title).to.be.a('string')
+                expect(response.body.ads[0].pictures).to.not.be.empty
+                expect(response.body.ads[0].pictures[0][0]).to.have.keys(['size', 'pictureUrl'])
+            })
     })
 })
